88694b046d147266e317b6d3fda849fc2e158ad0
Tetsuya Kusumoto <kusumot2.ya.o@gmail.com>
2020-04-15 11:45:01 +0900

    fix config.py

--- application/config.py
+++ application/config.py
@@ -2,26 +2,18 @@
 # -*- coding: utf-8 -*-
 
 
+host = "<host name>"
+dbname = "<database name>"
+port = "5432"
+user = "<user name>"
+password = "<password>"
 
-# host = "<host name>"
-# dbname = "<database name>"
-# port = "5432"
-# user = "<user name>"
-# password = "<password>"
-#
-# SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(user,
-#                                                                password,
-#                                                                host,
-#                                                                port,
-#                                                                dbname)
+SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(user,
+                                                               password,
+                                                               host,
+                                                               port,
+                                                               dbname)
 
-# SECRET_KEY = <seacret key>
-
-#Use local db
-SQLALCHEMY_DATABASE_URI = 'postgresql://t2kusumoto:otokareeiel@localhost:5432/mapsapp'
-DEBUG = True
-
+SECRET_KEY = <seacret key>
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-SECRET_KEY = b'g` \x83\xef]\xeaKl\x105-D\x96\x85A'
